0e78210536c4d97e888b33f52495ab208d94ca30
Padmaja Dhonddev <padmajadhonddev16@gmail.com>
2023-03-19 01:37:46 +0530

    ToDo App created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import { useState,useEffect } from "react";
+import List from "./components/List/list";
 
-function App() {
+const App=() =>{
+  const [work,setWork] = useState(false);
+  const [todoItem,setTodoItem] = useState(["Feed the dog","water the plants","Do shopping"]);
+  const [workItem,setWorkItem] = useState(["Learn DotNet","Learn React","Review PR"]);
+  const [input,setInput] = useState("");
+  const [apiData,setApiData] = useState(null)
+
+  const submit=(e)=>{
+    e.preventDefault();
+    work?setWorkItem([...workItem,input]):setTodoItem([...todoItem,input]);
+    setInput("");
+    console.log("button works")
+  }
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/todos").then((resp)=>resp.json()).then(data=>{
+      console.log(data)  
+      setApiData(data.slice(0,10))
+    });
+    // console.log(apiData)
+  },[])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div>
+        <label>Choose Item Type</label>
+        <input type={"checkbox"} onChange={e=>setWork(e.target.checked)} value={work} />
+      </div>
+      <div>
+        {
+          work?
+          (
+            <>
+            <h1>Work Items</h1>
+          <ul>
+            <List items={workItem} />
+          </ul>
+            </>
+          )
+          :
+          (
+            <>
+            <h1>Todo Items</h1>
+              <ul>
+                <List items={todoItem} />
+              </ul>
+            </>
+          )
+        }
+      </div>
+      <div>
+        <label>Enter your todo Item</label>
+        <input type="text" onChange={e=>setInput(e.target.value)} value={input}/>
+        <button onClick={e=>submit(e)}>Add</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/List/list.js
@@ -0,0 +1,7 @@
+const List = ({items})=>{
+
+
+    return items?.map(item=><li>{item}</li>)
+}
+
+export default List;
